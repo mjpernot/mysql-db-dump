e18f11714b8bff9b38b8223415f23dbf05ab7dca
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-01-08 13:35:09 +0000

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,89 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on "Keep a Changelog".  This project adheres to Semantic Versioning.
+
+
+## [3.0.1] - 2018-12-06
+### Changed
+- Documentation updates.
+
+
+## [3.0.0] - 2018-05-23
+Breaking Change
+
+### Changed
+- Changed "mysql_libs" calls to new naming schema.
+- Changed "cmds_gen" calls to new naming schema.
+- Changed "gen_libs" calls to new naming schema.
+- Changed "arg_parser" calls to new naming schema.
+- Changed function names from uppercase to lowercase.
+- Setup single-source version control.
+
+
+## [2.7.0] - 2018-05-11
+### Changed
+- Changed "server" to "mysql_class" module reference.
+- Changed "commands" to "mysql_libs" module reference.
+
+### Added
+- Added single-source version control.
+
+
+## [2.6.0] - 2017-08-18
+### Changed
+- Convert program to use local libraries from ./lib directory.
+- Change single quotes to double quotes.
+- Help_Message:  Replace docstring with printing the programs __doc__.
+
+
+## [2.5.0] - 2016-12-19
+### Changed
+- Set_Db_List:  Modified -B section to handle multiple databases.  Replaced sys.exit() with empty list.
+- Dump_DB:  Added elif to check for all-databases run and modified else to assume empty database list and there is nothing to dump or missing -D option.
+- Run_Program:  Removed if statement and call Set_Db_List function regardlessly, also added **kwargs to argument list to Set_Db_List call.
+- main:  Added opt_multi_list variable.
+- Added option for -B option to have multiple database names and also corrected an error if no option selected the program crashed trying to access the database name under -B option.
+
+
+## [2.4.0] - 2016-10-19
+### Changed
+- Crt_Dump_Cmd:  Replaced prog_gen.Is_Add_Cmd with cmds_gen.Is_Add_Cmd and Replaced prog_gen.Add_Cmd with cmds_gen.Add_Cmd.
+- Run_Program:  Determine if database list creation required.  Set the Servers class GTID mode.  Remove --set-gtid-purged for databases not GTID enabled.
+- Dump_Db:  If database list is empty, then run dump command without database names.  Replaced prog_gen.Add_Cmd with cmds_gen.Add_Cmd.
+- main:  Replaced Arg_Req_Xor with Arg_Xor_Dict and added -D option arguments to opt_dump_list variable.
+- MySQL 5.6 (GTID Enabled) gives warning if a database is dumped with GTID entries.  Added two new options (-D and -r) to purge the GTID entries from the dump file and/or dump all of the databases to a single dump file and this will drop the warning message.
+
+### Added
+- Dump_Cmd function.
+
+
+## [2.3.0] - 2016-10-18
+### Changed
+- main:  Changed -b to -B and -a to -A.
+- Set_Db_List:  Changed -b to -B.
+- Bring the main argument options in-line with the other programs.
+
+
+## [2.2.0] - 2016-10-12
+### Changed
+- Run_Program:  Replaced Crt_Srv_Inst with Create_Instance to allow for --defaults-extra-file option.
+- MySQL 5.6 now gives warning if password is passed on the command line.  To suppress this warning, will require the use of the --defaults-extra-file option.  This will require the use of updated commands library and server class files.  See in documentation above for exact version required for MySQL 5.6.
+
+
+## [2.1.0] - 2016-09-13
+### Changed
+- Dump_Db:  Replaced my_prog.Append_Cmd with prog_gen.Add_Cmd.  Replaced my_prog.Run_Prog_2_File with prog_gen.Run_Prog.
+- Crt_Dump_Cmd:  Replaced my_prog.Crt_Cmd with commands.Crt_Cmd.  Replaced my_prog.Append_Cmd with prog_gen.Add_Cmd.  Replaced my_prog.Is_Add_Cmd with prog_gen.Is_Add_Cmd.
+- main:  Replaced Arg_Parse with Arg_Parse2, reorganized the main 'if' statements, and streamlined the check process.
+- Set_Db_List:  Added connect and disconect commands to the database.
+
+
+## [2.0.0] - 2015-12-04
+### Changed
+- Extensive updates to the program to modularize and streamline the program and also replace the current database connection mechanism with a class based database connection mechanism.
+
+
+## [1.0.0] - 2015-10-08
+- Initial creation.
+
---
+++ mysql_db_dump.py
@@ -0,0 +1,309 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  mysql_db_dump.py
+
+    Description:  The mysql_db_dump program runs the mysqldump program against
+        a MySQL database and dumps one or more databases to files.
+
+    Usage:
+        mysql_db_dump.py -c file -d path {-B db_name [db_name ...] |
+            -A | -D} [-o name | -p path | -s | -z | -r] [-v | -h]
+
+    Arguments:
+        -B databases => Database names, space delimited.
+        -A => Dump all databases to individual files.
+        -D => Dump all databases to a single dump file.
+        -c file => Server configuration file.  Required arg.
+        -d dir path => Directory path to config file (-c). Required arg.
+        -o dir path => Directory path to dump directory.
+        -p dir path => Directory path to mysql programs.  Only required
+            if the mysql binary programs do not run properly.  (i.e. not
+            in the $PATH variable.)
+        -s => Run dump as a single transaction.
+        -r => Remove GTID entries from dump file.
+        -z => Compress database dump files
+        -v => Display version of this program.
+        -h => Help and usage message.
+
+        NOTE 1:  -v or -h overrides the other options.
+        NOTE 2:  -A, -B, and -D are XOR arguments.
+
+    Notes:
+        Database configuration file format (mysql_{host}.py):
+            # Configuration file for {Database Name/Server}
+            user = "root"
+            passwd = "ROOT_PASSWORD"
+            host = "IP_ADDRESS"
+            serv_os = "Linux" or "Solaris"
+            name = "HOSTNAME"
+            port = PORT_NUMBER (default of mysql is 3306)
+            cfg_file = "DIRECTORY_PATH/my.cfg"
+            sid = "SERVER_ID"
+            extra_def_file = "DIRECTORY_PATH/myextra.cfg"
+
+        NOTE:  Include the cfg_file even if running remotely as the file will
+            be used in future releases.
+
+        configuration modules -> name is runtime dependent as it can be
+            used to connect to different databases with different names.
+
+        Defaults Extra File format (filename.cfg):
+            [client]
+            password="ROOT_PASSWORD"
+            socket="DIRECTORY_PATH/mysql.sock"
+
+        NOTE:  The socket information can be obtained from the my.cnf
+            file under ~/mysql directory.
+
+    Example:
+        mysql_db_dump.py -A -c mysql -d config -z -s
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+
+# Local
+import lib.arg_parser as arg_parser
+import lib.gen_libs as gen_libs
+import lib.cmds_gen as cmds_gen
+import mysql_lib.mysql_class as mysql_class
+import mysql_lib.mysql_libs as mysql_libs
+import version
+
+# Version
+__version__ = version.__version__
+
+
+def help_message():
+
+    """Function:  help_message
+
+    Description:  Displays the program's docstring which is the help and usage
+        message when -h option is selected.
+
+    Arguments:
+
+    """
+
+    print(__doc__)
+
+
+def crt_dump_cmd(SERVER, args_array, opt_arg_list, opt_dump_list):
+
+    """Function:  crt_dump_cmd
+
+    Description:  Create the database dump command line.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (input) opt_arg_list -> List of commands to add to cmd line.
+        (input) opt_dump_list -> Dictionary of additional options.
+        (output) -> Database dump command line.
+
+    """
+
+    dump_args = mysql_libs.crt_cmd(SERVER, arg_parser.arg_set_path(args_array,
+                                                                   "-p") +
+                                   "mysqldump")
+
+    # Add arguments to dump command.
+    for arg in opt_arg_list:
+        dump_args = cmds_gen.add_cmd(dump_args, arg=arg)
+
+    # Append additional options to command.
+    return cmds_gen.is_add_cmd(args_array, dump_args, opt_dump_list)
+
+
+def dump_run(dump_cmd, dmp_file, compress, **kwargs):
+
+    """Function:  dump_run
+
+    Description:  Run the database dump command, save to file, and compress.
+
+    Arguments:
+        (input) dump_cmd -> Database dump command line.
+        (input) compress -> Compression flag.
+        (input) dmp_file -> Dump file and path name.
+        (input) **kwargs:
+            None
+
+    """
+
+    cmds_gen.run_prog(dump_cmd, ofile=dmp_file)
+
+    if compress:
+        gen_libs.compress(dmp_file)
+
+
+def dump_db(dump_cmd, db_list, compress, dmp_path):
+
+    """Function:  dump_db
+
+    Description:  Runs the database dump command against one or more databases
+        in the database list.  Will create a dump file for each database.
+
+    Arguments:
+        (input) dump_cmd -> Database dump command line.
+        (input) db_list -> Array of database names.
+        (input) compress -> Compression flag.
+        (input) dmp_path -> Database dump output directory path.
+
+    """
+
+    if db_list:
+
+        for db in db_list:
+            dump_cmd = cmds_gen.add_cmd(dump_cmd, arg=db)
+            dmp_file = gen_libs.crt_file_time(db, dmp_path, ".sql")
+
+            dump_run(dump_cmd, dmp_file, compress)
+
+            # Remove database name from command.
+            dump_cmd.pop(len(dump_cmd) - 1)
+
+    elif "--all-databases" in dump_cmd:
+        dmp_file = gen_libs.crt_file_time("All_Databases", dmp_path, ".sql")
+
+        dump_run(dump_cmd, dmp_file, compress)
+
+    else:
+        print("WARNING:  No databases to dump or missing -D option.")
+
+
+def set_db_list(SERVER, args_array, **kwargs):
+
+    """Function:  set_db_list
+
+    Description:  Get the database list and check if all databases or a single
+        database is being selected.
+
+    Arguments:
+        (input) SERVER -> Database server instance.
+        (input) args_array -> Array of command line options and values.
+        (output) -> Database list.
+        (input) **kwargs:
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    db_list = gen_libs.dict_2_list(mysql_libs.fetch_db_dict(SERVER),
+                                   "Database")
+
+    # Specified databases.
+    if "-B" in args_array:
+
+        # Difference of -B databases to database list.
+        for db in set(args_array["-B"]) - set(db_list):
+            print("Warning: Database(%s) does not exist." % (db))
+
+        # Intersect of -B databases to database list.
+        return list(set(args_array["-B"]) & set(db_list))
+
+    # All databases.
+    elif "-A" in args_array:
+        return db_list
+
+    # -D option or no option.
+    else:
+        return []
+
+
+def run_program(args_array, opt_arg_list, opt_dump_list, **kwargs):
+
+    """Function:  run_program
+
+    Description:  Creates class instance(s) and controls flow of the program.
+
+    Arguments:
+        (input) args_array -> Array of command line options and values.
+        (input) opt_arg_list -> List of commands to add to cmd line.
+        (input) opt_dump_list -> Dictionary of additional options.
+        (input) **kwargs:
+            multi_val -> List of options that may have multiple values.
+
+    """
+
+    SERVER = mysql_libs.create_instance(args_array["-c"], args_array["-d"],
+                                        mysql_class.Server)
+
+    SERVER.connect()
+    SERVER.set_srv_gtid()
+
+    dump_cmd = crt_dump_cmd(SERVER, args_array, opt_arg_list, opt_dump_list)
+
+    db_list = set_db_list(SERVER, args_array, **kwargs)
+
+    # Remove the -r option if database is not GTID enabled.
+    if "-r" in args_array and not SERVER.gtid_mode:
+        dump_cmd.remove(opt_dump_list["-r"])
+
+    compress = False
+
+    if "-z" in args_array:
+        compress = True
+
+    dmp_path = "./"
+
+    if "-o" in args_array:
+        dmp_path = args_array["-o"] + "/"
+
+    dump_db(dump_cmd, db_list, compress, dmp_path)
+
+    cmds_gen.disconnect([SERVER])
+
+
+def main():
+
+    """Function:  main
+
+    Description:  Initializes program-wide used variables and processes command
+        line arguments and values.
+
+    Variables:
+        dir_chk_list -> contains options which will be directories.
+        dir_crt_list -> contain options that require directory to be created.
+        opt_arg_list -> contains arguments to add to command line by default.
+        opt_dump_list -> contains optional arguments to mysqldump.
+        opt_multi_list -> contains the options that will have multiple values.
+        opt_req_list -> contains the options that are required for the program.
+        opt_val_list -> contains options which require values.
+        opt_xor_dict -> contains options which are XOR with its values.
+
+    Arguments:
+        (input) argv -> Arguments from the command line.
+
+    """
+
+    dir_chk_list = ["-o", "-d", "-p"]
+    dir_crt_list = ["-o"]
+    # --ignore-table=mysql.event -> Skips dumping the event table.
+    opt_arg_list = ["--ignore-table=mysql.event"]
+    opt_dump_list = {"-s": "--single-transaction",
+                     "-D": ["--all-databases", "--triggers", "--routines",
+                            "--events"],
+                     "-r": "--set-gtid-purged=OFF"}
+    opt_multi_list = ["-B"]
+    opt_req_list = ["-c", "-d"]
+    opt_val_list = ["-B", "-c", "-d", "-o", "-p"]
+    opt_xor_dict = {"-A": ["-B", "-D"], "-B": ["-A", "-D"], "-D": ["-A", "-B"]}
+
+    # Process argument list from command line.
+    args_array = arg_parser.arg_parse2(sys.argv, opt_val_list,
+                                       multi_val=opt_multi_list)
+
+    if not gen_libs.help_func(args_array, __version__, help_message):
+        if arg_parser.arg_xor_dict(args_array, opt_xor_dict) \
+           and not arg_parser.arg_require(args_array, opt_req_list) \
+           and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list,
+                                              dir_crt_list):
+            run_program(args_array, opt_arg_list, opt_dump_list,
+                        multi_val=opt_multi_list)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
