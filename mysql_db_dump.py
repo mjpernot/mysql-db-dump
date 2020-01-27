30557149eb6065f1c740077e6640cd6fdcc97606
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-01-27 11:49:10 +0000

    Update mysql_db_dump.main v3.1.0 - Refactored if statements

--- mysql_db_dump.py
+++ mysql_db_dump.py
@@ -303,8 +303,8 @@ def main():
                                        multi_val=opt_multi_list)
 
     if not gen_libs.help_func(args_array, __version__, help_message) \
-       and arg_parser.arg_xor_dict(args_array, opt_xor_dict) \
        and not arg_parser.arg_require(args_array, opt_req_list) \
+       and arg_parser.arg_xor_dict(args_array, opt_xor_dict) \
        and not arg_parser.arg_dir_chk_crt(args_array, dir_chk_list,
                                           dir_crt_list):
 
