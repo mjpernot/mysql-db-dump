f543b3337e765e0a34be533555668d91fee216d0
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-06 14:15:41 +0100

    Update test/unit/mysql_db_dump/run_program.py v3.2.0

--- test/unit/mysql_db_dump/run_program.py
+++ test/unit/mysql_db_dump/run_program.py
@@ -91,6 +91,8 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_w_option2 -> Test with -w option set to True.
+        test_w_option -> Test with -w option set to False.
         test_r_option_miss -> Test with -r option value not in command list.
         test_r_option2 -> Test with -r option and GTID set to False.
         test_r_option -> Test with -r option and GTID set to True.
@@ -120,11 +122,60 @@ class UnitTest(unittest.TestCase):
         self.args_array3 = {"-c": "config", "-d": "/dir", "-z": True}
         self.args_array4 = {"-c": "config", "-d": "/dir", "-z": False}
         self.args_array5 = {"-c": "config", "-d": "/dir", "-r": True}
+        self.args_array6 = {"-c": "config", "-d": "/dir", "-w": True}
         self.opt_arg_list = ["--ignore-table=mysql.event"]
         self.opt_dump_list = {
             "-s": "--single-transaction",
             "-D": ["--all-databases", "--triggers", "--routines", "--events"],
             "-r": "--set-gtid-purged=OFF"}
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_w_option2(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_w_option2
+
+        Description:  Test with -w option set to True.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(self.args_array6,
+                                                   self.opt_arg_list,
+                                                   self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_w_option(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_w_option
+
+        Description:  Test with -w option set to False.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(self.args_array,
+                                                   self.opt_arg_list,
+                                                   self.opt_dump_list))
 
     @unittest.skip("Bug:  If -r option value not in command list.")
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
