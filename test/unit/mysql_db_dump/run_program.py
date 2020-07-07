0e297c77d120a67a1fd08b8b855fdd93f48a8d3a
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-07-07 12:57:38 +0100

    Update test/unit/mysql_db_dump/run_program.py v3.2.0

--- test/unit/mysql_db_dump/run_program.py
+++ test/unit/mysql_db_dump/run_program.py
@@ -56,6 +56,7 @@ class Server(object):
         """
 
         self.gtid_mode = True
+        self.name = "ServerName"
 
     def connect(self):
 
@@ -90,6 +91,9 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Initialize testing environment.
+        test_email_subj -> Test with subject line passed.
+        test_email_no_subj -> Test with no subject line passed.
+        test_email -> Test with email configured.
         test_w_option2 -> Test with -w option set to True.
         test_w_option -> Test with -w option set to False.
         test_r_option_miss -> Test with -r option value not in command list.
@@ -122,11 +126,83 @@ class UnitTest(unittest.TestCase):
         self.args_array4 = {"-c": "config", "-d": "/dir", "-z": False}
         self.args_array5 = {"-c": "config", "-d": "/dir", "-r": True}
         self.args_array6 = {"-c": "config", "-d": "/dir", "-w": True}
+        self.args_array7 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"]}
+        self.args_array8 = {"-c": "config", "-d": "/dir", "-e": ["EmailAdr"],
+                            "t": ["Subject", "Line"]}
         self.opt_arg_list = ["--ignore-table=mysql.event"]
         self.opt_dump_list = {
             "-s": "--single-transaction",
             "-D": ["--all-databases", "--triggers", "--routines", "--events"],
             "-r": "--set-gtid-purged=OFF"}
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email_subj(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email_subj
+
+        Description:  Test with subject line passed.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array8, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email_no_subj(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email_no_subj
+
+        Description:  Test with no subject line passed.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array7, self.opt_arg_list, self.opt_dump_list))
+
+    @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.cmds_gen.disconnect",
+                mock.Mock(return_value=True))
+    @mock.patch("mysql_db_dump.set_db_list")
+    @mock.patch("mysql_db_dump.crt_dump_cmd")
+    @mock.patch("mysql_db_dump.mysql_libs.create_instance")
+    def test_email(self, mock_inst, mock_cmd, mock_list):
+
+        """Function:  test_email
+
+        Description:  Test with email configured.
+
+        Arguments:
+
+        """
+
+        mock_inst.return_value = self.server
+        mock_cmd.return_value = self.dump_cmd
+        mock_list.return_value = self.db_list
+
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array7, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -148,9 +224,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array6,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array6, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -172,9 +247,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
 
     @unittest.skip("Bug:  If -r option value not in command list.")
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
@@ -199,9 +273,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd2
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array5,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -225,9 +298,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array5,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -249,9 +321,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array5,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array5, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -273,9 +344,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array4,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array4, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -297,9 +367,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array3,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array3, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -321,9 +390,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array2,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array2, self.opt_arg_list, self.opt_dump_list))
 
     @mock.patch("mysql_db_dump.dump_db", mock.Mock(return_value=True))
     @mock.patch("mysql_db_dump.cmds_gen.disconnect",
@@ -345,9 +413,8 @@ class UnitTest(unittest.TestCase):
         mock_cmd.return_value = self.dump_cmd
         mock_list.return_value = self.db_list
 
-        self.assertFalse(mysql_db_dump.run_program(self.args_array,
-                                                   self.opt_arg_list,
-                                                   self.opt_dump_list))
+        self.assertFalse(mysql_db_dump.run_program(
+            self.args_array, self.opt_arg_list, self.opt_dump_list))
 
 
 if __name__ == "__main__":
